976519ad6d7213e07f215a4691a027f7877ff30a
Timo Friedl <timo@babelforce.com>
2025-07-07 00:02:50 +0200

    docs: init

--- src/rtvbp-spec/src/v1/metadata.rs
+++
@@ -1,4 +0,0 @@
-use indexmap::IndexMap;
-use serde_json::Value;
-
-pub type Metadata = IndexMap<String, Value>;
--- src/rtvbp-spec/src/v1/op/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod application;
-pub mod audio;
-pub mod log;
-pub mod ping;
-pub mod playback;
-pub mod recording;
-pub mod session;
